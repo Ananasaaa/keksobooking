f5dcddcde8fa1ad768ecb2d5e42d5c22b22cbe81
Ananasaaa <kairyakn@gmail.com>
2024-12-19 17:12:14 +0200

    fix constants

--- js/constants.js
+++ js/constants.js
@@ -1,52 +1,48 @@
-export const CONSTANTS = {
-  COORDS: {
-    MIN_X: 35.65000,
-    MAX_X: 35.70000,
-    MIN_Y: 139.70000,
-    MAX_Y: 139.80000,
-  },
-  PRICE: {
-    MIN: 1000,
-    MAX: 100000,
-  },
-  ROOMS: {
-    MIN: 1,
-    MAX: 5,
-  },
-  GUESTS: {
-    MIN: 1,
-    MAX: 10,
-  },
-  TYPES: ['palace', 'flat', 'house', 'bungalow'],
-  CHECK_TIMES: ['12:00', '13:00', '14:00'],
-  FEATURES: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-  PHOTOS: [
-    'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-    'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-    'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-  ],
-  TITLES: [
-    'Уютное жилье в Токио',
-    'Комфортная квартира в центре города',
-    'Роскошный дом с видом на океан',
-    'Квартира-студия с современным ремонтом',
-    'Просторный дом для большой семьи',
-    'Квартира рядом с парком',
-    'Дом с красивым садом',
-    'Уютное бунгало у моря',
-    'Апартаменты в историческом центре',
-    'Квартира с панорамным видом на город',
-  ],
-  DESCRIPTIONS: [
-    'Идеально подходит для семейного отдыха.',
-    'Вблизи магазинов и ресторанов.',
-    'Полностью меблировано.',
-    'Рядом с остановкой общественного транспорта.',
-    'Отличный вид на природу.',
-    'Современный интерьер.',
-    'Доступная цена.',
-    'Тихий и безопасный район.',
-    'Близко к центру города.',
-    'Подходит для долгосрочной аренды.',
-  ],
-};
+export const MIN_X = 35.65000;
+export const MAX_X = 35.70000;
+export const MIN_Y = 139.70000;
+export const MAX_Y = 139.80000;
+
+export const MIN_PRICE = 1000;
+export const MAX_PRICE = 100000;
+
+export const MIN_ROOMS = 1;
+export const MAX_ROOMS = 5;
+
+export const MIN_GUESTS = 1;
+export const MAX_GUESTS = 10;
+
+export const TYPES = ['palace', 'flat', 'house', 'bungalow'];
+export const CHECK_TIMES = ['12:00', '13:00', '14:00'];
+export const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+export const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+export const TITLES = [
+  'Уютное жилье в Токио',
+  'Комфортная квартира в центре города',
+  'Роскошный дом с видом на океан',
+  'Квартира-студия с современным ремонтом',
+  'Просторный дом для большой семьи',
+  'Квартира рядом с парком',
+  'Дом с красивым садом',
+  'Уютное бунгало у моря',
+  'Апартаменты в историческом центре',
+  'Квартира с панорамным видом на город',
+];
+
+export const DESCRIPTIONS = [
+  'Идеально подходит для семейного отдыха.',
+  'Вблизи магазинов и ресторанов.',
+  'Полностью меблировано.',
+  'Рядом с остановкой общественного транспорта.',
+  'Отличный вид на природу.',
+  'Современный интерьер.',
+  'Доступная цена.',
+  'Тихий и безопасный район.',
+  'Близко к центру города.',
+  'Подходит для долгосрочной аренды.',
+];
